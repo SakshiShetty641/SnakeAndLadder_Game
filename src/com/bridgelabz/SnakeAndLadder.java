ca2efaa7166f8672eed9fcfc6938048442bcbc3a
Sakshi <sakshishetty1231997@gmail.com>
2021-08-04 22:40:19 +0530

    Added: Welcome Message to the Program

---
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+/**
+ * @author Sakshi Shetty
+ * Purpose - To simulate a Snake and Ladder Game 
+ */
+public class SnakeAndLadder {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Snake and Ladder Game");
+	}
+}
