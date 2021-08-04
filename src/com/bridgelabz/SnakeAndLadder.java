0b7687f3a5b431fbcff7c78f034e741f54417274
Sakshi <sakshishetty1231997@gmail.com>
2021-08-04 22:47:55 +0530

    Added: Player starts from position 0

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,10 +1,12 @@
 package com.bridgelabz;
 /**
  * @author Sakshi Shetty
- * Purpose - To simulate a Snake and Ladder Game 
+ * Purpose - Player starts with position 0
  */
 public class SnakeAndLadder {
 	public static void main(String[] args) {
+		int Player1 = 0;
 		System.out.println("Welcome to the Snake and Ladder Game");
+		System.out.println("Your position is:" + Player1);
 	}
 }
