564ee9293171ef21ec3a53004a08c06b2f5bff9a
Sakshi <sakshishetty1231997@gmail.com>
2021-08-04 23:00:14 +0530

    Added: To roll a dice and generate number between 1 to 6

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,12 +1,16 @@
 package com.bridgelabz;
 /**
  * @author Sakshi Shetty
- * Purpose - Player starts with position 0
+ * Purpose - Player rolls a dice to get number between 1 to 6
  */
 public class SnakeAndLadder {
 	public static void main(String[] args) {
 		int Player1 = 0;
 		System.out.println("Welcome to the Snake and Ladder Game");
-		System.out.println("Your position is:" + Player1);
+		/**
+		 * Using Math.random() function to generate random between 1 to 6
+		 */
+		int Dice = (int) (Math.random() * 6 + 1);
+		System.out.println("The Player's Position after rolling the die is : " + Dice);
 	}
 }
