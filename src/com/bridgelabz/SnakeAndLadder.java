3bbce2b2974efabfc295124f9a1df4ba86db2235
Sakshi <sakshishetty1231997@gmail.com>
2021-08-06 12:16:26 +0530

    Added: While loop to repeat the game till player reaches winning position

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,38 +1,55 @@
 package com.bridgelabz;
 /**
  * @author Sakshi Shetty
- * Purpose - Player rolls a dice to and checks whether option is no-play, ladder or snake 
+ * Purpose - Player rolls a dice to and repeats till player reaches winning position 100
  */
 public class SnakeAndLadder {
+	public static final int WINNING_POSITION = 100;
+	public static int PLAYER_POSITION = 0;
+
 	public static void main(String[] args) {
-		int Player_Position = 0;
 		int ladder = 1;
 		int snake = 2;
 		int noPlay = 3;
 		System.out.println("Welcome to the Snake and Ladder Game");
-		/**
-		 * Using Math.random() function to generate random between 1 to 6
-		 */
-		int Dice = (int) (Math.random() * 6 + 1);
-		System.out.println("The Player's Position after rolling the die is : " + Dice);
 
 		/**
-		 * Using Math.random() function to generate random between 1 to 3 for user to get options
+		 * Using while loop to repeat the game till player reaches exact winning
+		 * position Using Math.random to generate random numbers between 1 to 6
 		 */
-		int playerCheck = (int) Math.floor(Math.random() * 3 + 1);
+		while (PLAYER_POSITION < WINNING_POSITION) {
+			int Dice = (int) (Math.random() * 6 + 1);
+			System.out.println("The Player's die number is : " + Dice);
 
-		switch (playerCheck) {
-		case 1:
-			Player_Position = Player_Position + Dice;
-			System.out.println("The player position is :" + Player_Position);
-			break;
-		case 2:
-			Player_Position = Player_Position - Dice;
-			System.out.println("The player position is :" + Player_Position);
-			break;
-		case 3:
-			Player_Position = 0;
-			System.out.println("There is no play and the player position is :" + Player_Position);
+			/**
+			 * Using Math.random() function to generate random between 1 to 3 for user to
+			 * get options
+			 */
+			int playerCheck = (int) Math.floor(Math.random() * 3 + 1);
+
+			switch (playerCheck) {
+			case 1:
+				PLAYER_POSITION = PLAYER_POSITION + Dice;
+				System.out.println("The player position after ladder is :" + PLAYER_POSITION);
+				break;
+			case 2:
+				PLAYER_POSITION = PLAYER_POSITION - Dice;
+				System.out.println("The player position after snake is :" + PLAYER_POSITION);
+				break;
+			case 3:
+				PLAYER_POSITION = 0;
+				System.out.println("There is no play and the player position is :" + PLAYER_POSITION);
+
+				/**
+				 * The player position will be set to zero if the player goes below zero
+				 */
+
+				if (PLAYER_POSITION < 0) {
+					System.out.println("You need to start again from starting position");
+					PLAYER_POSITION = 0;
+				}
+			}
+
 		}
 	}
 }
