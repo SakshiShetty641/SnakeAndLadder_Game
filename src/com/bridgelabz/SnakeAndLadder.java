5696b72c6f05c350ee360d9cc36bef775cbefe63
Sakshi <sakshishetty1231997@gmail.com>
2021-08-06 11:45:33 +0530

    Added: User options for player to get no-play, snake or ladder

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,16 +1,38 @@
 package com.bridgelabz;
 /**
  * @author Sakshi Shetty
- * Purpose - Player rolls a dice to get number between 1 to 6
+ * Purpose - Player rolls a dice to and checks whether option is no-play, ladder or snake 
  */
 public class SnakeAndLadder {
 	public static void main(String[] args) {
-		int Player1 = 0;
+		int Player_Position = 0;
+		int ladder = 1;
+		int snake = 2;
+		int noPlay = 3;
 		System.out.println("Welcome to the Snake and Ladder Game");
 		/**
 		 * Using Math.random() function to generate random between 1 to 6
 		 */
 		int Dice = (int) (Math.random() * 6 + 1);
 		System.out.println("The Player's Position after rolling the die is : " + Dice);
+
+		/**
+		 * Using Math.random() function to generate random between 1 to 3 for user to get options
+		 */
+		int playerCheck = (int) Math.floor(Math.random() * 3 + 1);
+
+		switch (playerCheck) {
+		case 1:
+			Player_Position = Player_Position + Dice;
+			System.out.println("The player position is :" + Player_Position);
+			break;
+		case 2:
+			Player_Position = Player_Position - Dice;
+			System.out.println("The player position is :" + Player_Position);
+			break;
+		case 3:
+			Player_Position = 0;
+			System.out.println("There is no play and the player position is :" + Player_Position);
+		}
 	}
 }
