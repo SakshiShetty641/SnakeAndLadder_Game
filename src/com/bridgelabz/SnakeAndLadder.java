86d29e9ca183a24dba243c8808927d71e5c9bb66
Sakshi <sakshishetty1231997@gmail.com>
2021-08-07 15:47:46 +0530

    Added: To count the number of times the dice was rolled

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,7 +1,7 @@
 package com.bridgelabz;
 /**
  * @author Sakshi Shetty
- * Purpose - Player rolls a dice to and repeats till player reaches winning position 100
+ * Purpose - To count the number of times the dice was rolled
  */
 public class SnakeAndLadder {
 	public static final int WINNING_POSITION = 100;
@@ -11,7 +11,7 @@ public class SnakeAndLadder {
 		int ladder = 1;
 		int snake = 2;
 		int noPlay = 3;
-		int toWin;
+		int count = 0;
 		System.out.println("Welcome to the Snake and Ladder Game");
 
 		/**
@@ -20,6 +20,7 @@ public class SnakeAndLadder {
 		 */
 		while (PLAYER_POSITION < WINNING_POSITION) {
 			int Dice = (int) (Math.random() * 6 + 1);
+			count++;
 			System.out.println("The Player's die number is : " + Dice);
 
 			/**
@@ -27,31 +28,31 @@ public class SnakeAndLadder {
 			 * get options
 			 */
 			int playerCheck = (int) Math.floor(Math.random() * 3 + 1);
-			toWin = WINNING_POSITION - PLAYER_POSITION;
-			if (toWin > Dice) {
-				switch (playerCheck) {
-				case 1:
+			switch (playerCheck) {
+			case 1:
+				if ((PLAYER_POSITION + Dice) > 100)
+					PLAYER_POSITION = PLAYER_POSITION;
+				else
 					PLAYER_POSITION = PLAYER_POSITION + Dice;
-					System.out.println("The player position after ladder is :" + PLAYER_POSITION);
-					break;
-				case 2:
-					PLAYER_POSITION = PLAYER_POSITION - Dice;
-					System.out.println("The player position after snake is :" + PLAYER_POSITION);
-					break;
-				case 3:
+				break;
+			case 2:
+				PLAYER_POSITION = PLAYER_POSITION - Dice;
+				System.out.println("The player position after snake is :" + PLAYER_POSITION);
+				break;
+			case 3:
+				PLAYER_POSITION = 0;
+				System.out.println("There is no play and the player position is :" + PLAYER_POSITION);
+
+				/**
+				 * The player position will be set to zero if the player goes below zero
+				 */
+
+				if (PLAYER_POSITION < 0) {
+					System.out.println("You need to start again from starting position");
 					PLAYER_POSITION = 0;
-					System.out.println("There is no play and the player position is :" + PLAYER_POSITION);
-
-					/**
-					 * The player position will be set to zero if the player goes below zero
-					 */
-
-					if (PLAYER_POSITION < 0) {
-						System.out.println("You need to start again from starting position");
-						PLAYER_POSITION = 0;
-					}
 				}
 			}
 		}
+		System.out.println("Number of times die rolled: " + count);
 	}
 }
